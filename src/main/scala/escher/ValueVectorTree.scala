68cd9ee7387f5e8babed76634f192d8101246a14
JiayiWei <wjydzh1@163.com>
2017-04-16 10:04:39 +0800

    Add ValueVectorTree: efficient data structure for searching against partial value vector

---
+++ src/main/scala/escher/ValueVectorTree.scala
@@ -0,0 +1,152 @@
+package escher
+
+import collection.mutable
+import ValueVectorTree._
+import escher.Synthesis.{ValueMap, ValueVector}
+
+/**
+  * A tree with fixed depth, each path from root to leaf represents a ValueVector,
+  * can be used to efficiently find ValueVectors that match a goal (partial vector)
+  */
+object ValueVectorTree {
+
+  sealed trait TreeNode[A]
+
+  case class LeafNode[A](term: A) extends TreeNode[A]
+
+
+  class InternalNode[A](val children: mutable.Map[TermValue, TreeNode[A]]) extends TreeNode[A] {
+    def addTerm(term: A, valueVector: List[TermValue]): Boolean = valueVector match {
+      case Nil => throw new Exception("Empty valueVector!")
+      case List(v) =>
+        if(children.contains(v)) false
+        else {
+          children(v) = LeafNode(term)
+          true
+        }
+      case v :: tail =>
+        val n1 = children.get(v) match {
+          case Some(n1: InternalNode[A]) => n1
+          case None => new InternalNode[A](mutable.Map())
+          case _ => throw new Exception()
+        }
+        n1.addTerm(term, tail)
+        children(v) = n1
+        true
+    }
+
+    def searchTerms(valueVector: List[Either[Unit ,TermValue]]): Iterator[A] = valueVector match {
+      case Nil => throw new Exception("Empty valueVector!")
+      case List(v) => v match {
+        case Left(_) =>
+          children.values.toIterator.collect {
+            case LeafNode(t) => t
+          }
+        case Right(tv) =>
+          children.get(tv) match {
+            case Some(LeafNode(t)) => Iterator(t)
+            case _ => Iterator()
+          }
+      }
+      case v :: tail => v match {
+        case Left(_) =>
+          children.values.toIterator.flatMap {
+            case n1: InternalNode[A] =>
+              n1.searchTerms(tail)
+            case _ => throw new Exception()
+          }
+        case Right(tv) =>
+          children.get(tv) match {
+            case Some(n1: InternalNode[A]) => n1.searchTerms(tail)
+            case _ => Iterator()
+          }
+      }
+    }
+
+    def searchATerm(valueVector: List[Either[Unit ,TermValue]]): Option[A] = valueVector match {
+      case Nil => throw new Exception("Empty valueVector!")
+      case List(v) => v match {
+        case Left(_) =>
+          children.values.foreach {
+            case LeafNode(t) => return Some(t)
+            case _ => sys.error("ill-formed tree.")
+          }
+          None
+        case Right(tv) =>
+          children.get(tv) match {
+            case Some(LeafNode(t)) => Some(t)
+            case _ => None
+          }
+      }
+      case v :: tail => v match {
+        case Left(_) =>
+          children.values.foreach {
+            case n1: InternalNode[A] =>
+              n1.searchATerm(tail).foreach(t => return Some(t))
+            case _ => throw new Exception()
+          }
+          None
+        case Right(tv) =>
+          children.get(tv) match {
+            case Some(n1: InternalNode[A]) => n1.searchATerm(tail)
+            case _ => None
+          }
+      }
+    }
+
+  }
+
+  def print[T](tree: TreeNode[T], show: T => String, indent: Int): Unit = tree match {
+    case LeafNode(term) =>
+      println("  " * indent + s"* ${show(term)}")
+    case in : InternalNode[T] =>
+      in.children.foreach{
+        case (tv, tree1) =>
+          println("  " * indent + s"- ${tv.show}")
+          print(tree1, show, indent + 1)
+      }
+  }
+
+}
+
+/**
+  * A tree with fixed depth, each path from root to leaf represents a ValueVector,
+  * can be used to efficiently find ValueVectors that match a goal (partial vector)
+  * @param depth the length of each ValueVector
+  */
+class ValueVectorTree[A](depth: Int){
+  private var _size = 0
+  def size: Int = _size
+
+  val root = new InternalNode[A](mutable.Map())
+
+  def addTerm(term: A, valueVector: ValueVector): Boolean = {
+    val added = root.addTerm(term, valueVector.toList)
+    if(added) _size += 1
+    added
+  }
+
+  private def valueMapToVector(valueMap: ValueMap): List[Either[Unit, TermValue]] ={
+    (0 until depth).toList.map{ i =>
+      valueMap.get(i) match{
+        case Some(tv) => Right(tv)
+        case None => Left(())
+      }
+    }
+  }
+
+  def searchTerms(valueMap: ValueMap): Iterator[A] = {
+    val vv = valueMapToVector(valueMap)
+    root.searchTerms(vv)
+  }
+
+  def searchATerm(valueMap: ValueMap): Option[A] = {
+    val vv = valueMapToVector(valueMap)
+    root.searchATerm(vv)
+  }
+
+  def printRoot(show: A => String): Unit ={
+    println("---Root Status---")
+    print(root, show, 0)
+  }
+}
---
+++ src/test/scala/escher/ValueVectorTreeTests.scala
@@ -0,0 +1,47 @@
+package escher
+
+import org.scalatest.WordSpec
+import DSL._
+
+
+class ValueVectorTreeTests extends WordSpec{
+  "a tree of string" should {
+    val tree = new ValueVectorTree[String](4)
+    tree.addTerm("A", IS(1,2,3,4))
+    tree.addTerm("B", IS(1,2,6,8))
+
+
+
+    "has size 2" in {
+      assert{
+        tree.size === 2
+      }
+    }
+
+    "find A and B" in {
+      assert{
+        tree.searchTerms(Map(0->1,1->2,2->3,3->4)).toList === List("A")
+      }
+
+      assert{
+        tree.searchATerm(Map(0->1,1->2,2->3,3->4)).get === "A"
+      }
+
+      assert{
+        tree.searchTerms(Map(0 ->1, 1->2, 2->6, 3->8)).toList === List("B")
+      }
+
+      assert{
+        tree.searchATerm(Map(0 ->1, 1->2, 2->6, 3->8)).get === "B"
+      }
+
+      assert{
+        tree.searchTerms(Map(0 -> 1, 1 -> 2)).toSet === Set("A","B")
+      }
+
+      assert{
+        Set("A","B") contains tree.searchATerm(Map(0 -> 1, 1 -> 2)).get
+      }
+    }
+  }
+}
