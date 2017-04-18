09056d8a2103097e0fdcf4505535c684d6b2b000
JiayiWei <wjydzh1@163.com>
2017-04-18 10:52:23 +0800

    Add new time tools

--- src/main/scala/escher/TimeTools.scala
+++ src/main/scala/escher/TimeTools.scala
@@ -4,11 +4,47 @@ package escher
   * Running time related tools
   */
 object TimeTools {
+  type Nanosecond = Long
+
   def printTimeUsed[A](taskName: String)(task: => A): A = {
-    val t1 = System.currentTimeMillis()
-    val result = task
-    val time = System.currentTimeMillis() - t1
-    println(s"*** [$taskName] time used: $taskName: ${time}ms ***")
+    val (nano, result) = measureTime(task)
+    val time = nano/1000000.0
+    println(s"*** [$taskName] time used: %.2fms ***".format(time))
     result
+  }
+
+  def measureTime[A](task: => A): (Nanosecond, A) = {
+    val t1 = System.nanoTime()
+    val result = task
+    val time = System.nanoTime() - t1
+    (time, result)
+  }
+
+  /** Since this method uses Thread.sleep, it may not be accurate for methods with very short running time */
+  def scaleUpRunningTime[A](factor: Int)(task: => A): A = {
+    require(factor >= 1)
+
+    if(factor == 1) return task
+
+    val (nano, result) = measureTime(task)
+    val extraNano = (factor - 1) * nano
+    val millis = extraNano / 1000000
+    val nanos = extraNano - millis * 1000000
+    Thread.sleep(millis, nanos.toInt)
+    result
+  }
+
+  @inline
+  def runOnce[A](f: => A): A = {
+    f
+  }
+
+  @inline
+  def run5Times[A](f: => A): A = {
+    f
+    f
+    f
+    f
+    f
   }
 }
